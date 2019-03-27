2f73a6f78d3ca80145736303da107a8fc19ec9f8
Mir Ahmed <mir.ahmed39@yahoo.com>
2019-03-27 18:56:54 -0400

    formatting date for user profile view.

--- application_code/routes/profile.js
+++ application_code/routes/profile.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
 // Profile
 router.get('/profile', function (req, res){
   if (req.user) {
-    res.render('profile', {user: req.user});
+    // extracting month and year from the date.
+    const monthIndex = req.user.dateRegistered.getMonth();
+    const date = monthNames[monthIndex] + " " + req.user.dateRegistered.getFullYear();
+    console.log(date);
+    res.render('profile', {user: req.user, date: date});
   } else {
     res.render('login_required');
   }
