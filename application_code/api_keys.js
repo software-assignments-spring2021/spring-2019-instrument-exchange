dcbea8a18f1d2bc35072f6a481432ef7b174d164
Mir Ahmed <mir.ahmed39@yahoo.com>
2019-04-05 18:04:14 -0400

    adding api key for zipcode location service.

--- application_code/api_keys.js
+++ application_code/api_keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   maps_javascript: "AIzaSyAjBNLfIMAPbum-F-PsgsLcu2cvLwDgSgY",
-  places: "AIzaSyAjBNLfIMAPbum-F-PsgsLcu2cvLwDgSgY"
+  places: "AIzaSyAjBNLfIMAPbum-F-PsgsLcu2cvLwDgSgY",
+  zipAPI: "sJwxksPBYrUSWHPlPriS0DdBvwbOKI8wqdXPQU9A9QHJp5lEkeK0hgNDvan0B8MD"
 };
