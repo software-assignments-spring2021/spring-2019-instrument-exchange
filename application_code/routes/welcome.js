8f93857d3fc15beba9208e5613987a9c655e06dd
Mir Ahmed <mir.ahmed39@yahoo.com>
2019-04-09 13:10:05 -0400

    fixing error message that shows up when homepage is loaded.

--- application_code/routes/welcome.js
+++ application_code/routes/welcome.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-   if(!req.user){
-      res.redirect('login');
-   }
-   res.render('buy_sell',{user:req.user});
-  
+   if(req.user) res.render('buy_sell', {user: req.user});
+   else res.redirect('login');
+
 });
 
 module.exports = router;
