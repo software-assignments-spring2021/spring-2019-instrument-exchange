af034a49063a786ef2a85978fa99fa0872b2ab61
Mir Ahmed <mir.ahmed39@yahoo.com>
2019-04-15 16:28:09 -0400

    debugging for selected option for instruments and studios

--- application_code/public/javascript/location_service.js
+++ application_code/public/javascript/location_service.js
@@ -2,8 +2,7 @@ $( document ).ready(function() {
     $(".service").change(function() {
        // getting value from both slider and price.
        $("#slider-value").val($("#zip-slider").val() + " miles");
-       // const $priceSort = $("#price").val();
-       // console.log($distance);
-       // console.log($priceSort);
     });
+    console.log($("#price").val());
+
 });
