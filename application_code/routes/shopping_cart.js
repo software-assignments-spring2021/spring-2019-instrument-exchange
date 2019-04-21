8c74bf43b23c50b1dc8103fb1d5a0021fe0fa36b
Jonathan <jw4107@nyu.edu>
2019-04-21 16:02:37 -0400

    Added BootStrap alert for checkout

--- application_code/routes/shopping_cart.js
+++ application_code/routes/shopping_cart.js
@@ -9,9 +9,17 @@ router.get('/shopping_cart', function (req, res, next) {
     res.render("shopping_cart", {cart: req.session.cart});
 });
 
+// Proceed to Checkout button
 router.post('/checkout', function (req, res) {
     if (req.user) {
       res.render("checkout", {cart: req.session.cart, user: req.user});
+    } else res.render("login_required");
+});
+
+// Place Your Order Button
+router.post('/place_order', function (req, res) {
+    if (req.user) {
+      res.render("buyer_portal", {message: "Your order has been successfully placed!"});
     } else res.render("login_required");
 });
 
