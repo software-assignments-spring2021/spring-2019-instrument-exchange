742a1ee81a84acd9a412ecc039ca098279b6c5b8
Jonathan <jw4107@nyu.edu>
2019-05-02 12:50:41 -0400

    Fixed a bug with Order schema

--- application_code/db.js
+++ application_code/db.js
@@ -106,7 +106,8 @@ const OrderSchema = new Schema({
     userId: {type: Schema.Types.ObjectId, ref: "User"},
     cart: {type: Object, required: true},
     address: {type: String, required: true},
-    name: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
     paymentId: {type: String, required: true},
 });
 
--- application_code/routes/shopping_cart.js
+++ application_code/routes/shopping_cart.js
@@ -49,6 +49,7 @@ router.post('/place_order', function (req, res) {
        });
 
        order.save(function(err, result) {
+         console.log(order);
            req.flash('success', 'Successfully bought product!');
            req.session.cart = null;
            res.redirect('/');
